e438602418c7d840418939d17fc21d9482ecc39c
RaphaelS1 <raphaelsonabend@gmail.com>
2024-11-25 14:07:33 +0000

    resolve AB comments

--- book/experiments/code.R
+++ book/experiments/code.R
@@ -104,7 +104,7 @@ ggsave("book/Figures/evaluation/calibD.png", g, height = 3, units = "in",
   dpi = 600)
 
 
-## Random forests
+## Decision trees
 rm(list = ls())
 set.seed(20241104)
 
@@ -137,6 +137,47 @@ png("book/Figures/forests/cars.png", height = 400, width = 600)
 rattle::fancyRpartPlot(fit, caption = "")
 dev.off()
 
+# Log-rank test
+rm(list = ls())
+set.seed(20241125)
+library(survival)
+library(ggplot2)
+library(patchwork)
+library(survminer)
+sf0 <- survfit(Surv(time, status) ~ 1, lung)
+p0 <- ggplot(rbind(data.frame(x=sf0$time, y=sf0$surv), data.frame(x=0,y=1)), aes(x=x,y=y))+geom_step()
+
+opt_1 <- lung$age > 50
+opt_2 <- lung$age > 75
+df_1 <- cbind(lung, split = opt_1)
+df_2 <- cbind(lung, split = opt_2)
+logrank_1 <- survdiff(Surv(time, status) ~ split, data = df_1)
+logrank_2 <- survdiff(Surv(time, status) ~ split, data = df_2)
+sf_1 <- survfit(Surv(time, status) ~ split, df_1)
+sf_2 <- survfit(Surv(time, status) ~ split, df_2)
+p1 <- ggplot(
+  rbind(data.frame(x=sf_1$time, y=sf_1$surv,group=c(rep(FALSE, sf_1$strata[1]), rep(TRUE, sf_1$strata[2]))), data.frame(x = 0, y = 1, group=c(TRUE, FALSE))),
+  aes(x=x,y=y,color=group)) +
+  geom_step() +
+  geom_label(aes(x=x,y=y), data.frame(x = 750, y = 0.9), label =sprintf("χ²=%.2f (p=%.2f)", logrank_1$chisq, logrank_1$pvalue), inherit.aes = FALSE)
+p2 <- ggplot(
+  rbind(data.frame(x=sf_2$time, y=sf_2$surv,group=c(rep(FALSE, sf_2$strata[1]), rep(TRUE, sf_2$strata[2]))), data.frame(x = 0, y = 1, group=c(TRUE, FALSE))),
+  aes(x=x,y=y,color=group)) +
+  geom_step() +
+  geom_label(aes(x=x,y=y), data.frame(x = 750, y = 0.9), label = sprintf("χ²=%.2f (p=%.2f)", logrank_2$chisq, logrank_2$pvalue), inherit.aes = FALSE)
+
+g <- p0 / (p1 + p2) &
+  labs(x = "t", y = "S(t)") &
+  theme_classic() &
+  guides(color = "none") &
+  scale_x_continuous(limits = c(0, 1000),  expand = c(0, 0)) &
+  plot_annotation(tag_levels = 'a', tag_suffix = ")")
+
+ggsave("book/Figures/forests/logrank.png", g, height = 6, units = "in",
+  dpi = 600)
+
+
+# Random forest plot
 rm(list = ls())
 set.seed(20241109)
 library(survival)
@@ -161,10 +202,15 @@ df = data.frame(x = x, y = c(y1, y2, y3), group = group)
 p0 <- ggplot(df, aes(x = x, y = y, color = group))
 p1 <-  p0 + geom_step()
 p2 <- p0 + geom_vline(xintercept = x, lty = 2, color = "gray") + geom_step()
-p3 <- p0 + geom_vline(xintercept = x, lty = 2, color = "gray") +  geom_point()
+p3 <- p0 + geom_vline(xintercept = x, lty = 2, color = "gray") +  geom_point() +
+  geom_label(aes(x=x,y=y),
+    data.frame(x=5,y=c(0.9, 0.2, 0.4),group=c("blue", "red", "green")),
+    label = sprintf("S(6) = %s", c(1, 0.3, 0.5)))
 
 df2 = data.frame(x = x, y = apply(data.frame(y1, y2, y3), 1, mean))
-p4 <- ggplot(df2, aes(x = x, y = y)) + geom_step() + geom_point()
+p4 <- ggplot(df2, aes(x = x, y = y)) + geom_step() + geom_point() +
+  geom_label(aes(x=x,y=y),
+    data.frame(x=6,y=0.5), label = "S(6) = 0.6")
 
 ybreaks = seq.int(0, 1, 0.25)
 xbreaks = seq.int(0, 12, 3)
@@ -176,5 +222,5 @@ g = p1 + p2 + p3 + p4 &
   scale_x_continuous(limits = c(0, 14), breaks = xbreaks, labels = xbreaks, expand = c(0, 0)) &
   plot_annotation(tag_levels = 'a', tag_suffix = ")")
 
-ggsave("book/Figures/forests/bootstrap.png", g, height = 5, units = "in",
+ggsave("book/Figures/forests/bootstrap.png", g, height = 6, units = "in",
   dpi = 600)
